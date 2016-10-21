53a128c8485d0787effdc54d2b638bdd58f93964
Erwin Müller <erwin.mueller@deventm.org>
2016-10-21 12:02:14 +0200

    Update license headers.

--- sscontrol-osgi-unix/src/main/java/com/anrisoftware/sscontrol/cmd/internal/core/ArgsMap.java
+++ sscontrol-osgi-unix/src/main/java/com/anrisoftware/sscontrol/cmd/internal/core/ArgsMap.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
+ *
+ * This file is part of sscontrol-osgi-unix.
+ *
+ * sscontrol-osgi-unix is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * sscontrol-osgi-unix is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with sscontrol-osgi-unix. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.anrisoftware.sscontrol.cmd.internal.core;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
